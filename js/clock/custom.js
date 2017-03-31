eddeb204ff0d158cab384c558360b9b8a3e0c045
deuscode <deuscode87@gmail.com>
2017-03-31 00:34:35 -0400

    First Commit

---
+++ js/clock/custom.js
@@ -0,0 +1,121 @@
+/******************************************************************************************************************************
+CLOCK
+*******************************************************************************************************************************/
+$(document).ready(function() {
+// Create two variables with names of months and days of the week in the array
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// Create an object newDate()
+var newDate = new Date();
+// Retrieve the current date from the Date object
+newDate.setDate(newDate.getDate());
+// At the output of the day, date, month and year    
+
+
+setInterval( function() {
+    // Create an object newDate () and extract the second of the current time
+    var seconds = new Date().getSeconds();
+    // Add a leading zero to the value of seconds
+    $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds + '<h1></H1><p>Seconds</p>');
+    },1000);
+    
+setInterval( function() {
+    // Create an object newDate () and extract the minutes of the current time
+    var minutes = new Date().getMinutes();
+    // Add a leading zero to the minutes
+    $("#min").html(( minutes < 10 ? "0" : "" ) + minutes + '<h1></H1><p>Minutes</p>');
+    },1000);
+    
+setInterval( function() {
+    // Create an object newDate () and extract the clock from the current time
+    var hours = new Date().getHours();
+    // Add a leading zero to the value of hours
+    $("#hours").html(( hours < 10 ? "0" : "" ) + hours + '<h1></H1><p>Hours</p>');
+    }, 1000);
+    
+}); 
+/******************************************************************************************************************************
+ANIMATIONS
+*******************************************************************************************************************************/
+(function($) {
+    "use strict";
+    var isMobile = false;
+    if (navigator.userAgent.match(/Android/i) || 
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) || 
+        navigator.userAgent.match(/iPad/i)|| 
+        navigator.userAgent.match(/iPod/i) || 
+        navigator.userAgent.match(/BlackBerry/i)) {                 
+        isMobile = true;            
+    }
+    if (isMobile == true) {
+        $('body').removeClass('nomobile');
+        $('.animated').removeClass('animated');
+    }
+    $(function() {
+        if (isMobile == false) {
+            $('*[data-animated]').addClass('animated');
+        }
+        function animated_contents() {
+            $(".animated:appeared").each(function (i) {
+                var $this    = $(this),
+                    animated = $(this).data('animated');
+
+                setTimeout(function () {
+                    $this.addClass(animated);
+                }, 50 * i);
+            });
+        }
+        animated_contents();
+        $(window).scroll(function () {
+            animated_contents();
+        });
+    });
+})(jQuery);
+/******************************************************************************************************************************
+SLIDER
+*******************************************************************************************************************************/
+(function($) {
+    "use strict";
+    $("body.nomobile #slider").revolution(
+    {
+            delay:9000,
+            startheight:450,
+            startwidth:890,
+
+            thumbWidth:100,
+            thumbHeight:50,
+            thumbAmount:5,
+
+            onHoverStop:"on",
+            hideThumbs:200,
+            navigationType:"bullet",
+            navigationStyle:"round",
+            navigationArrows:"none",
+
+            touchenabled:"on",
+
+            navOffsetHorizontal:0,
+            navOffsetVertical:80,
+            shadow:undefined,
+            fullWidth:"on",
+            fullScreen:"on"
+    });
+})(jQuery);
+/******************************************************************************************************************************
+BOOTSTRAP
+*******************************************************************************************************************************/
+(function($) {
+    "use strict";
+        $('[data-rel=tooltip]').tooltip();
+        $(".alert").alert();
+})(jQuery);
+/******************************************************************************************************************************
+PROGRESS BAR
+*******************************************************************************************************************************/
+(function($) {
+    "use strict";
+    $("a.btn-progress").click(function(){
+        $('#bar-container').slideToggle(); 
+    });
+})(jQuery);
